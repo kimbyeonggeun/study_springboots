5223d5af4261318887ccfbe8b0110e9b1f6418f1
kimbyeonggeun <qudrms2157@gmail.com>
2023-01-18 10:33:20 +0900

    modify homecontroller

--- src/main/java/com/study/study_springboots/controller/HomeController.java
+++ src/main/java/com/study/study_springboots/controller/HomeController.java
@@ -6,30 +6,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.study.study_springboots.dao.HomeDao;
 
-// @Controller // 서블렛의 프론트로 인식하는것 처럼 됨
+@Controller // 서블렛의 프론트로 인식하는것 처럼 됨
 public class HomeController {
     @Autowired
     HomeDao homeDao;
 
-    // @RequestMapping(value = "/homejsp") // @urlpattern과 같은 역할
+    @RequestMapping(value = "/homejsp") // @urlpattern과 같은 역할
     public String homejsp() {
         int i = 0;
         return "home";
     }
 
-    // @RequestMapping(value = "/homehtml") // @urlpattern과 같은 역할
+    @RequestMapping(value = "/homehtml") // @urlpattern과 같은 역할
     public String homehtml() {
         int i = 0;
         return "home";
     }
 
-    // @RequestMapping(value = "/home") // @urlpattern과 같은 역할
+    @RequestMapping(value = "/home") // @urlpattern과 같은 역할
     public void home() {
         Object result = homeDao.getList();
         int i = 0;
     }
 
-    // @RequestMapping(value = { "", "/", "/main" }) // url이 3개중에 하나라도 만족되면 이 함수 실행
+    @RequestMapping(value = { "", "/", "/main" }) // url이 3개중에 하나라도 만족되면 이 함수 실행
     public String main() {
         int i = 0;
         return "main";
